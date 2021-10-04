3b1dd67d9e1258b7fec86905e9fae3d90f8b17d9
junghyunseo0128a <90805571+junghyunseo0128a@users.noreply.github.com>
2021-10-05 00:52:31 +0900

    update

---
+++ WelcomeBack.js
@@ -0,0 +1,16 @@
+class WelcomeBack extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <h2>hello</h2>
+            </>
+        )
+    }
+}
