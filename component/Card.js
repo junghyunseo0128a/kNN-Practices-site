624c7168d2d535885c18d007c0c8e87f6c55b8b4
welcomehyunseo <48713654+welcomehyunseo@users.noreply.github.com>
2021-10-05 05:55:01 +0900

    update

---
+++ component/Card.js
@@ -0,0 +1,12 @@
+function Card(props) {
+    return (
+        <div class='Card'>
+            <div class="m-3 px-3 py-3 border border-secondary rounded" id="featured-3">
+                <h2 class="border-bottom">Columns with icons</h2>
+                <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
--- js/navbar_login_event.js
+++
@@ -1,9 +0,0 @@
-const logined_element = document.getElementById('logined');
-const not_logined_element = document.getElementById('not_logined');
-
-var login_state_bool = false;
-
-if(login_state_bool) 
-    logined_element.hidden = true;
-else
-    not_logined_element.hidden = true;
