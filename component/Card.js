0544d7661701ccdee498b588e4a9ee403da26746
junghyunseo0128a <90805571+junghyunseo0128a@users.noreply.github.com>
2021-10-05 09:09:07 +0900

    update

--- component/Card.js
+++ component/Card.js
@@ -1,8 +1,8 @@
 function Card(props) {
     return (
         <div class='Card'>
-            <div class="m-3 px-3 py-3 border border-secondary rounded" id="featured-3">
-                <h2 class="border-bottom">Columns with icons</h2>
+            <div class="container-xl mb-3 px-3 py-3 border border-secondary rounded">
+                <h4 class="border-bottom">{props.title}</h4>
                 <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
                     
                 </div>
