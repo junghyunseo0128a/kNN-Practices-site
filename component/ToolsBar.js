82d8c2db07e655653174a585908a0e989c23d698
welcomehyunseo <48713654+welcomehyunseo@users.noreply.github.com>
2021-10-07 01:22:31 +0900

    update

---
+++ component/ToolsBar.js
@@ -0,0 +1,16 @@
+function ToolsBar(props) {
+    return (
+        <div class='ToolsBar'>
+            <div class='left'>
+                <a href='index.html'>Home</a>
+                <a href='#'>My projects</a>
+                <a href='#'>Documents</a>
+            </div>
+
+            <div class='right'>
+                <button>Sign in</button>
+                <button>Sign up</button>
+            </div>
+        </div>
+    );
+}
