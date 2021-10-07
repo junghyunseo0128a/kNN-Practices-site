62e2b0c9f9133ce3cfc9fd76cd357c8cae624360
junghyunseo0128a <90805571+junghyunseo0128a@users.noreply.github.com>
2021-10-07 19:39:01 +0900

    update

--- component/TitleBar.js
+++ component/TitleBar.js
@@ -1,10 +1,10 @@
 function TitleBar(props) {
     return (
         <div class='TitleBar'>
-            <svg xmlns="http://www.w3.org/2000/svg" class="" width="40" height="40" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M17 4h3v16h-3V4zM5 14h3v6H5v-6zm6-5h3v11h-3V9z" />
             </svg>
-            <h2>kNN Practice site.</h2>
+            <h1>kNN Practice site.</h1>
         </div>
     );
 }
